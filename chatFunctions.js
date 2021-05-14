a772c8999ff1d78662405ceb7b2000a3b8050555
Jacob <jengopockets@gmail.com>
2021-05-14 02:41:02 -0500

    rps

--- chatFunctions.js
+++ chatFunctions.js
@@ -1,4 +1,5 @@
 const tmi = require('tmi.js');
+const { rockPaper } = require('./rps')
 require('dotenv').config()
 function connectChat(channelName, prevChannel) {
     
@@ -24,22 +25,35 @@ function connectChat(channelName, prevChannel) {
     client.connect();
     
     client.on('connected', (address, port) => {
-        client.action(channelName, `${prevChannel} sent you a Luvbot! Luvbot9001 Gives @${channelName} a big hug! Type !luv <channel> to send Luvbot to someone else!`)
+        client.action(channelName, `@jengopockets sent you a Luvbot! Type !rps <r,p,or s> to play Rock,Paper,Scissors!`)
     });
 
     client.on('message', (channel, userState, message, self) => {
         const cmd = message.split(" ")
-        if (cmd[0] === '!luv'){
-            let passName = cmd[1]
-            // console.log("first", passName, passName[0])
-            if (passName[0] === '@'){
-                passName = passName.slice(1)
-                // console.log("Change?", passName)
+        // if (cmd[0] === '!luv'){
+        //     let passName = cmd[1]
+        //     // console.log("first", passName, passName[0])
+        //     if (passName[0] === '@'){
+        //         passName = passName.slice(1)
+        //         // console.log("Change?", passName)
+        //     }
+        //     client.say (channelName, `I will go send love to ${cmd[1]}! Bye!`)
+        //     client.disconnect()
+        //     connectChat(passName, channelName)
+        // }
+        if(message === `!rps r` || message === '!rps s' || message === '!rps p'){
+            const [result, cpu] = rockPaper(message[5]);
+            let entry = null
+            if (message[5] === 'r'){
+                entry = 'Rock'
+            }else if(message[5] === 's'){
+                entry = 'Scissors'
+            }else{
+                entry = 'Paper'
             }
-            client.say (channelName, `I will go send love to ${cmd[1]}! Bye!`)
-            client.disconnect()
-            connectChat(passName, channelName)
-        }
+     
+            client.action(process.env.CHANNEL, `You Chose ${entry} , Your Opponent Chose ${cpu}! (${result})` );
+         }
     })
 
     
